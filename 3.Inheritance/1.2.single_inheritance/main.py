3191e148213f1db470074654a32ba29fe451bf38
Anelle22 <anelleed@gmail.com>
2026-03-02 13:44:40 +0000

    More exercises on OOP

---
+++ 3.Inheritance/1.2.single_inheritance/main.py
@@ -0,0 +1,8 @@
+from project.animal import Animal
+from project.dog import Dog
+
+a = Animal()
+d = Dog()
+
+print(d.bark())
+print(d.eat())
---
+++ 3.Inheritance/1.4.multilevel_inheritence/main.py
@@ -0,0 +1,7 @@
+from project.sports_car import SportsCar
+
+c = SportsCar()
+
+print(c.move())
+print(c.drive())
+print(c.race())
